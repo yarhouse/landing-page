04e5b019c4317a529c7e38ca7d09db1f2654c869
Aaron Yarhouse <yarhouse.aaron@gmail.com>
2016-10-30 16:09:40 -0400

    re-init commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name personalPortfolioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the personalPortfolioApp
+ */
+angular.module('personalPortfolioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
